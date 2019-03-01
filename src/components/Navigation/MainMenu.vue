a2096d5912451904a72d0df41e765ca303876050
Audrey Loiseau <audrey.loiseau17@gmail.com>
2019-03-01 19:28:40 +0100

    Add MainMenu component to navigate between main pages

---
+++ src/components/Navigation/MainMenu.vue
@@ -0,0 +1,24 @@
+<template>
+	<nav>
+		<ul>
+			<li>
+				<nuxt-link to="/movies/to-buy">To Buy</nuxt-link>
+			</li>
+			<li>
+				<nuxt-link to="/movies/own">Own</nuxt-link>
+			</li>
+			<li>
+				<nuxt-link to="/movies/to-see">To see</nuxt-link>
+			</li>
+			<li>
+				<nuxt-link to="/movies">Seen</nuxt-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'MainMenu'
+}
+</script>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,8 +1,17 @@
 <template>
 	<div>
+		<main-menu />
 		<nuxt/>
 	</div>
 </template>
+
+<script>
+import MainMenu from '~/components/Navigation/MainMenu'
+
+export default {
+	components: { MainMenu }
+}
+</script>
 
 <style>
 html {

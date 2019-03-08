600fe70670c94e2682d863988f8f9b119619b04b
Audrey Loiseau <audrey.loiseau17@gmail.com>
2019-03-08 20:58:55 +0100

    Add movies filters forms and default page axios parameters

---
+++ src/components/Movies/Filters.vue
@@ -0,0 +1,98 @@
+<template>
+	<div>
+		<h3>Filtrez-nous !</h3>
+
+		<form @submit.prevent="filter">
+			<div>
+				<label for="director">
+					Director
+				</label>
+				<input
+					id="director"
+					v-model="filters.director"
+					name="director"
+					type="text">
+			</div>
+
+			<div>
+				<label for="types">
+					Types
+				</label>
+				<input
+					id="types"
+					v-model="filters.types"
+					name="types"
+					type="text">
+			</div>
+
+			<div>
+				<p>Rating</p>
+				<input
+					id="fantastic"
+					v-model="filters.rating"
+					name="rating"
+					value="fantastic"
+					type="radio">
+				<label for="fantastic">
+					Fantastic
+				</label>
+				<input
+					id="bad"
+					v-model="filters.rating"
+					name="rating"
+					value="bad"
+					type="radio">
+				<label for="bad">
+					Bad
+				</label>
+				<button 
+					:checked="!filters.rating"
+					type="button"
+					@click="filters.rating = null">
+					Clear
+				</button>
+			</div>
+
+			<div>
+				<label for="firstLetter">
+					First letter
+				</label>
+				<input
+					id="firstLetter"
+					v-model="filters.firstLetter"
+					name="firstLetter"
+					type="text">
+			</div>
+
+			<div>
+				<input
+					type="submit"
+					value="Filter">
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Filters',
+	data() {
+		return {
+			filters: {
+				rating: null,
+				types: null,
+				director: null,
+				firstLetter: null
+			},
+			errors: null
+		}
+	},
+	methods: {
+		filter() {
+			// todo emit params
+			console.log('filter')
+			this.$emit('filter-movies', this.filters)
+		}
+	}
+}
+</script>
--- src/pages/movies/to-buy/index.vue
+++ src/pages/movies/to-buy/index.vue
@@ -14,35 +14,61 @@
 		<p v-if="!movies.length">
 			No movies found.
 		</p>
+		
+		<!-- Filters -->
+		<filters @filter-movies="filterMovies"/>
 	</div>
 </template>
 
 <script>
 import MovieItem from '~/components/Movies/MovieItem'
+import Filters from '~/components/Movies/Filters'
 
 export default {
-	components: { MovieItem },
+	components: { MovieItem, Filters },
 	data() {
 		return {
 			movies: [],
-			currentPage: 1
+			currentPage: 1,
+			defaultParams: {}
 		}
 	},
-	asyncData({ params, $axios }) {
+	asyncData({ $axios }) {
+		// Define parameters use for first movie catch
+		// and then pagination and filter
+		const defaultParams = {
+			number: 10,
+			possession_state: 'to_own'
+		}
+
 		return $axios
 			.$get('/api/movies/search', {
-				params: {
-					number: 10,
-					possession_state: 'to_own'
-				}
+				params: defaultParams
 			})
 			.then(res => {
 				return {
 					currentPage: res.current_page,
-					movies: res.data ? res.data : []
+					movies: res.data ? res.data : [],
+					defaultParams
 				}
 			})
 			.catch(error => console.log(error))
+	},
+	methods: {
+		filterMovies(data) {
+			// Combine default and filter parameters
+			const params = Object.assign({}, this.defaultParams, data)
+
+			return this.$axios
+				.$get('/api/movies/search', {
+					params: params
+				})
+				.then(res => {
+					this.currentPage = res.current_page
+					this.movies = res.data ? res.data : []
+				})
+				.catch(error => console.log(error))
+		}
 	}
 }
 </script>
